fc3f239dac1a9fbf157917f4cf825a2cbd1ef05a
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-04-22 11:36:19 +0530

    update done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+function openNav() {
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
 
 
 
